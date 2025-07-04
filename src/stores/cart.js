2bbd3da23d781cfa604f119d2fbb9bef2b2fe784
PauloK3tchup <passandoalimpo13@gmail.com>
2025-07-04 10:03:36 -0300

    first commit

---
+++ src/stores/cart.js
@@ -0,0 +1,18 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cart', () => {
+  const cart = ref({
+    items: [],
+    total: 0,
+  })
+  function addToCart(book) {
+    this.cart.items.push(book)
+    this.cart.total += book.price
+    console.log(this.cart)
+  }
+  return {
+    cart,
+    addToCart,
+  }
+})
